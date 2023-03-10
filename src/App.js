835579d425e2658633974d16698ab6503bda7300
Alyssa Cervantes (she/her) <112597248+alyssaleanne98@users.noreply.github.com>
2023-03-10 14:48:27 -0800

    components created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import Cards from '..components/Cards'
+import axios from "axios";
 
+
+const API_URL = "http://localhost:3000/api/v1/cards";
+
+function getAPIData() {
+  return axios.get(API_URL).then((response) => response.data)
+
+}
 function App() {
+  const [cards, setCards] = useState([]);
   return (
     <div className="App">
-      
+      <h1>Hello</h1>
     </div>
   );
 }
